2fcadd00890459d4dcc94a7fa86126b6867fd265
Akhil Reddy <153515972+AkhilReddyModugu@users.noreply.github.com>
2024-11-29 10:50:50 +0530

    Update main.py

--- fastapi-tutorial/main.py
+++ fastapi-tutorial/main.py
@@ -6,7 +6,6 @@ from pymongo import MongoClient
 
 
 app=FastAPI()
-# conn= MongoClient("mongodb+srv://akhilreddymodugu13:reddyakhil13@cluster0.opexl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 
 
